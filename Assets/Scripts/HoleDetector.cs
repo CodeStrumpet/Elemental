288fdb24eee4e7e4fba0b00e6b51743447962fef
unknown <Matt Sonic@MattSonic-PC.(none)>
2012-03-08 20:33:57 -0800

    added birds

---
+++ Assets/Scripts/HoleDetector.cs
@@ -0,0 +1,152 @@
+using UnityEngine;
+using System.Collections;
+
+public class HoleDetector : MonoBehaviour {
+    public Transform colliderMesh;
+    public Vector2 bounds = new Vector2(10, 10);
+    public Vector2 res = new Vector2(16, 8);
+    public float boxHeight = 1.0f;
+    public float rayDistance = 20.0f;
+    public int frameSkip = 5;
+    public bool debugRaycast = true;
+    public bool debugBoxes = true;
+    public float holeDelta = 0.1f;
+    public Transform volcanoPrefab;
+    public int maxVolcanos = 3;
+
+    Transform[] colliders;
+    int frameCount = 0;
+    Transform[] volcanos;
+
+    void Start () {
+	GameObject box = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+	Vector2 spacing = new Vector2(bounds.x / res.x,
+				      bounds.y / res.y);
+	Vector2 boxOffset = new Vector2(-spacing.x * 0.5f,
+					-spacing.y * 0.5f);
+	Vector2 objOffset = new Vector2(-bounds.x * 0.5f + spacing.x,
+					-bounds.y * 0.5f + spacing.y);
+
+	colliders = new Transform[(int)res.x * (int)res.y];
+	volcanos = new Transform[maxVolcanos];
+	// Create Volcanos
+	for (int i = 0; i < maxVolcanos; i++) {
+	    Vector3 offscreen = new Vector3(100, 100, 100);
+	    volcanos[i] = Instantiate(volcanoPrefab,
+				      offscreen,
+				      Quaternion.identity) as Transform;
+	}
+
+	// Create Boxes
+	box.renderer.enabled = false;
+	box.transform.localScale = new Vector3(spacing.x, boxHeight, spacing.y);
+	for (int y = 0; y < res.y; y++) {
+	    for (int x = 0; x < res.x; x++) {
+		Transform newBox = Instantiate(box.transform,
+					       new Vector3(x * spacing.x, 0,
+							   y * spacing.y),
+					       Quaternion.identity) as Transform;
+		newBox.Translate(boxOffset.x, -boxHeight * 0.5f, boxOffset.y);
+		Destroy(newBox.GetComponent<BoxCollider>());
+		newBox.parent = transform;
+		colliders[y * (int)res.x + x] = newBox;
+	    }
+	}
+	transform.Translate(objOffset.x, 0, objOffset.y);
+	GameObject.Destroy(box);
+    }
+	
+    void Update () {
+	frameCount++;
+	if (frameCount >= frameSkip) {
+	    frameCount = 0;
+	} else {
+	    return;
+	}
+
+	int layer = colliderMesh.gameObject.layer;
+	
+	// Move all volcanos offscreen
+	for (int i = 0; i < maxVolcanos; i++) {
+	    Vector3 offscreen = new Vector3(100, 100, 100);
+	    volcanos[i].position = offscreen;
+	}
+
+	int volcanoIndex = 0;
+	for (int y = 0; y < res.y; y++) {
+	    for (int x = 0; x < res.x; x++) {
+		Transform box = colliders[y * (int)res.x + x];
+		box.renderer.enabled = debugBoxes;
+		MoveBox(box, 1 << layer);
+		if (IsHole(x, y)) {
+		    // Move volcanos back onscreen
+		    box.renderer.material.color = Color.red;
+		    volcanos[volcanoIndex].position = box.position;
+		    volcanoIndex++;
+		    if (volcanoIndex > maxVolcanos - 1) {
+			volcanoIndex = maxVolcanos - 1;
+		    }
+		} else {
+		    box.renderer.material.color = Color.grey; 
+		}
+	    }
+	}
+    }
+
+    void MoveBox(Transform box, int layerMask) {
+	Vector3 origin = new Vector3(box.position.x,
+				     rayDistance,
+				     box.position.z);
+	RaycastHit hit;
+	bool rayHit = Physics.Raycast(origin,
+				      new Vector3(0, -1, 0),
+				      out hit,
+				      rayDistance, layerMask);
+	if (rayHit) {
+	    if (debugRaycast) {
+		// Draw green hit ray
+		Debug.DrawRay(origin, new Vector3(0, -rayDistance, 0), Color.green);
+	    }
+	    Vector3 newPos = new Vector3(box.position.x,
+					 hit.point.y - boxHeight * 0.5f,
+					 box.position.z);
+	    box.position = newPos;
+	} else {
+	    if (debugRaycast) {
+		// Draw non-hit ray
+		Debug.DrawRay(origin, new Vector3(0, -rayDistance, 0));
+	    }
+	    box.position = new Vector3(box.position.x, -boxHeight, box.position.z);
+	}
+    }
+    
+    bool IsHole(int x, int y) {
+	if (x == 0 || y == 0 || x >= res.x || y >= res.y) {
+	    // Don't check edges
+	    return false;
+	}
+	Transform box = colliders[y * (int)res.x + x];
+	Transform north = colliders[(y - 1) * (int)res.x + x];
+	Transform south = colliders[(y + 1) * (int)res.x + x];
+	Transform east = colliders[y * (int)res.x + x - 1];
+	Transform west = colliders[y * (int)res.x + x + 1];
+	return (IsWall(box, north) &&
+		IsWall(box, south) &&
+		IsWall(box, east) &&
+		IsWall(box, west));
+    }
+
+    bool IsWall(Transform box, Transform other) {
+	return (other.position.y - box.position.y >= holeDelta);
+    }
+
+    void OnDrawGizmos() {
+	Gizmos.color = Color.blue;
+	Gizmos.DrawWireCube(Vector3.zero, new Vector3(bounds.x,
+						      boxHeight,
+						      bounds.y));
+    }
+
+
+}
