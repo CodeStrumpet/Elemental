cd5a6e48a1882b36418c8fd6046dfde4952fc247
Cory Barr <cory.barr@gmail.com>
2012-05-28 19:56:14 -0700

    added script to have particle emitter represent Kinect data

---
+++ Assets/Scripts/KinectParticles/EnableDisable.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class EnableDisable : MonoBehaviour {
+	
+	public ParticleEmitter pe;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
